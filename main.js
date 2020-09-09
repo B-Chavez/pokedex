e6ec95dd4c34ecaa8d29aaa499c2fb6ca0695251
b-chavez <brandedhard@gmail.com>
2020-09-09 12:25:25 -0700

    added more pics, simplified some of the code

--- main.js
+++ main.js
@@ -2,32 +2,54 @@ var bulbasaur = document.querySelector(".bulbasaur");
 var pokemonImageContainer = document.querySelector(".pokeImgContainer");
 var charmander = document.querySelector(".charmander");
 var squirtle = document.querySelector(".squirtle");
+var createPokemonImage = document.createElement("img");
 
 bulbasaur.addEventListener('click', function (){changeImage("bulbasaur")})
 charmander.addEventListener('click', function (){changeImage("charmander")})
 squirtle.addEventListener('click', function (){changeImage("squirtle")})
 
 function changeImage(pokemon){
-    var createPokemonImage = document.createElement("img")
+
     
     if(pokemonImageContainer.querySelector(".pokemon")){
         pokemonImageContainer.removeChild(document.querySelector(".pokemon"))
     }
     
     if(pokemon === "bulbasaur"){
-        createPokemonImage.setAttribute("src", "./Bulbasaur.png")
-        createPokemonImage.setAttribute("class", "pokemon")
-        pokemonImageContainer.appendChild(createPokemonImage)
-        return;
+        makePokemonImage(pokemon)
+    } else if(pokemon === "ivysaur"){
+        makePokemonImage(pokemon)
+    } else if(pokemon === "venusaur"){
+        makePokemonImage(pokemon)
     } else if(pokemon === "charmander"){
-        createPokemonImage.setAttribute("src", "./Charmander.png")
-        createPokemonImage.setAttribute("class", "pokemon")
-        pokemonImageContainer.appendChild(createPokemonImage)
-        return;
+        makePokemonImage(pokemon)
+    } else if(pokemon === "charmeleon"){
+        makePokemonImage(pokemon)
+    } else if(pokemon === "charizard"){
+        makePokemonImage(pokemon)
     } else if(pokemon === "squirtle"){
-        createPokemonImage.setAttribute("src", "./Squirtle.png")
+        makePokemonImage(pokemon)
+    } else if(pokemon === "wartortle"){
+        makePokemonImage(pokemon)
+    } else if(pokemon === "blastoise"){
+        makePokemonImage(pokemon)
+    }
+}
+
+
+var greenButton = document.querySelector(".greenbutton")
+greenButton.addEventListener('click',function (){clearScreen()})
+
+function clearScreen(){
+    if(document.querySelector(".pokemon")){
+        pokemonImageContainer.removeChild(document.querySelector(".pokemon"))
+        document.querySelector(".pokemonImgName").textContent = ""
+    }
+}
+
+function makePokemonImage(pokemon){
+        createPokemonImage.setAttribute("src", "./images/"+ pokemon+ ".png")
         createPokemonImage.setAttribute("class", "pokemon")
         pokemonImageContainer.appendChild(createPokemonImage)
-    }
-    return;
+        document.querySelector(".pokemonImgName").textContent = pokemon
 }
